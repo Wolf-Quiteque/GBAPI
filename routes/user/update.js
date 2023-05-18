bc4d691e4f30353d92dcd72f42e13d8d646e982b
Wolf-Quiteque <marcioqui3@gmail.com>
2023-05-18 10:32:46 +0100

    nene

--- routes/user/update.js
+++ routes/user/update.js
@@ -17,4 +17,21 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/post", async (req, res) => {
+  try {
+    const cliente = await clientPromise;
+    const db = cliente.db("GBUSINESS");
+    await db
+      .collection("usuarios")
+      .updateOne(
+        { telefone: req.body.telefone },
+        { $push: { posts: req.body } }
+      );
+
+    res.status(200).json("true");
+  } catch (err) {
+    res.status(500).json(false);
+  }
+});
+
 module.exports = router;
