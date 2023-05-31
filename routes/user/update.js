fac4c5b5436a2b7c8904a4062040a136eca89158
Marcio Ragy Daveiga Quiteque <33323596+Wolf-Quiteque@users.noreply.github.com>
2023-05-31 22:06:41 +0000

    news

--- routes/user/update.js
+++ routes/user/update.js
@@ -24,7 +24,7 @@ router.post("/post", async (req, res) => {
     await db
       .collection("posts")
       .insertOne(
-        { req.body }
+         req.body 
       );
 
     res.status(200).json("true");
